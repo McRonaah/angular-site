ff5e8b3c71b40ac6f801ccae8023997d412d3abc
McRonaah <ronaldcheruiyot342@gmail.com>
2023-03-14 10:49:11 +0300

    :fire: :feat: fix components arrangement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/pages/1home/home-page/home-page.component';
+import { AboutPageComponent } from './components/pages/2About/about-page/about-page.component';
+import { ServicesPageComponent } from './components/pages/4Services/services-page/services-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'services', component: ServicesPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(
-    routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './components/pages/home-page/home-page.component';
-import { AboutPageComponent } from './components/pages/about-page/about-page.component';
-import { ServicesPageComponent } from './components/pages/services-page/services-page.component';
-import { NavbarComponent } from './components/pages/navbar/navbar.component';
+import { HomePageComponent } from './components/pages/1home/home-page/home-page.component';
+import { AboutPageComponent } from './components/pages/2About/about-page/about-page.component';
+import { ServicesPageComponent } from './components/pages/4Services/services-page/services-page.component';
+import { NavbarComponent } from './components/pages/0navbar/navbar.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomePageComponent },
